7713f160fe7e2be837c9f90ffe38ba949052afca
dan <dan@cs4p.net>
2021-07-29 10:06:40 -0400

    Fixed bug with #emails in Inbox reported after each folder filter is applied

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_folder_filter(server, folder_name):
     email_filter_list = []
     for msgid, data in server.fetch(messages, ['ENVELOPE']).items():
         envelope = data[b'ENVELOPE']
-        for addr in envelope.sender:
+        for addr in envelope.from_:
             if addr not in email_filter_list:
                 email_filter_list.append(str(addr))
     logging.info(' '.join(['Found', str(len(email_filter_list)), 'email addresses for', folder_name]))
@@ -35,7 +35,7 @@ def apply_folder_filters(server, messages, email_list, folder_name):
     counter = 0
     for msgid, data in server.fetch(messages, ['ENVELOPE']).items():
         envelope = data[b'ENVELOPE']
-        for addr in envelope.sender:
+        for addr in envelope.from_:
             if str(addr) in email_list:
                 server.move(msgid, folder_name)
                 counter = counter + 1
@@ -45,6 +45,7 @@ def apply_folder_filters(server, messages, email_list, folder_name):
 def run():
     logging.basicConfig(filename='imap_cleaner.log', level=logging.INFO,
                         format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info('###################################################################################################')
     logging.info('Starting operation...')
     server = server_login(imap_server, imap_user, imap_password)
     select_info = server.select_folder('INBOX')
@@ -57,6 +58,7 @@ def run():
         logging.info('Looking for new messages to move to ' + fldr)
         p = apply_folder_filters(server, new_messages, email_list, fldr)
         logging.info(' '.join(['Moved', str(p), 'messages to', fldr]))
+        select_info = server.select_folder('INBOX')
         logging.info('%d messages in INBOX' % select_info[b'EXISTS'])
     logging.info('Finished moving messages. %d messages remain in INBOX' % select_info[b'EXISTS'])
     server.logout()
